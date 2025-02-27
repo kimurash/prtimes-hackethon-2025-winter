d69dc6bd3f128d3cd607dca1387b4b2bed475a68
shunsei <k.shunsei1227@gmail.com>
2025-02-27 21:07:15 +0900

    Remove unnecessary comment

--- frontend/src/pages/dreams/mine/MyDreamCards.tsx
+++ frontend/src/pages/dreams/mine/MyDreamCards.tsx
@@ -11,32 +11,31 @@ const MyDreamCards = ({ myDreams }: MyDreamCardsProps) => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-8">
         {myDreams.map((dream, index) => (
           <AlertDialog.Root key={index}>
-          <AlertDialog.Trigger>
-            <div className="border bg-white rounded-xl shadow-lg p-4 min-h-48 py-6">
-              <p className="text-gray-800 overflow-hidden text-ellipsis">
-                {dream.title}
-                {dream.is_public && (
-                <p className="text-green-500 font-medium mt-2 text-sm border-t pt-5 text-center">
-                  いいね {dream.likes}
+            <AlertDialog.Trigger>
+              <div className="border bg-white rounded-xl shadow-lg p-4 min-h-48 py-6">
+                <p className="text-gray-800 overflow-hidden text-ellipsis">
+                  {dream.title}
+                  {dream.is_public && (
+                    <p className="text-green-500 font-medium mt-2 text-sm border-t pt-5 text-center">
+                      いいね {dream.likes}
+                    </p>
+                  )}
                 </p>
-              )}
-              </p>
-            </div>
-          </AlertDialog.Trigger>
-          <AlertDialog.Content className="flex flex-col justify-between min-h-[300px] max-w-[400px]">
-            <AlertDialog.Description size="4">
-              {dream.content}
-            </AlertDialog.Description>
-            <Flex gap="3" justify="center">
-              {/* mt 削除 */}
-              <AlertDialog.Cancel>
-                <Button variant="soft" color="gray">
-                  閉じる
-                </Button>
-              </AlertDialog.Cancel>
-            </Flex>
-          </AlertDialog.Content>
-        </AlertDialog.Root>
+              </div>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content className="flex flex-col justify-between min-h-[300px] max-w-[400px]">
+              <AlertDialog.Description size="4">
+                {dream.content}
+              </AlertDialog.Description>
+              <Flex gap="3" justify="center">
+                <AlertDialog.Cancel>
+                  <Button variant="soft" color="gray">
+                    閉じる
+                  </Button>
+                </AlertDialog.Cancel>
+              </Flex>
+            </AlertDialog.Content>
+          </AlertDialog.Root>
         ))}
       </div>
     </>
