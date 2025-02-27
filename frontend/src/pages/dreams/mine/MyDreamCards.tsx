c917adede4c479d87e8d17cfa6a707cac427f519
cercil0605 <kuamaichi2223@gmail.com>
2025-02-27 20:57:18 +0900

    add: like on my screen

--- frontend/src/pages/dreams/mine/MyDreamCards.tsx
+++ frontend/src/pages/dreams/mine/MyDreamCards.tsx
@@ -1,3 +1,4 @@
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { Dream } from "../../../types/dream";
 
 interface MyDreamCardsProps {
@@ -9,17 +10,33 @@ const MyDreamCards = ({ myDreams }: MyDreamCardsProps) => {
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-8">
         {myDreams.map((dream, index) => (
-          <div key={index} className="border bg-white rounded-xl shadow-lg p-4 min-h-48 py-6 relative">
-            <p className="text-gray-800 overflow-hidden text-ellipsis">
-              {dream.title}
-            </p>
-            <p className="text-gray-600 mt-2">{dream.content}</p>
-            {dream.is_public && (
-              <p className="text-green-500 font-medium mt-2 text-sm border-t pt-5 text-center">
-                いいね {dream.likes}
+          <AlertDialog.Root key={index}>
+          <AlertDialog.Trigger>
+            <div className="border bg-white rounded-xl shadow-lg p-4 min-h-48 py-6">
+              <p className="text-gray-800 overflow-hidden text-ellipsis">
+                {dream.title}
+                {dream.is_public && (
+                <p className="text-green-500 font-medium mt-2 text-sm border-t pt-5 text-center">
+                  いいね {dream.likes}
+                </p>
+              )}
               </p>
-            )}
-          </div>
+            </div>
+          </AlertDialog.Trigger>
+          <AlertDialog.Content className="flex flex-col justify-between min-h-[300px] max-w-[400px]">
+            <AlertDialog.Description size="4">
+              {dream.content}
+            </AlertDialog.Description>
+            <Flex gap="3" justify="center">
+              {/* mt 削除 */}
+              <AlertDialog.Cancel>
+                <Button variant="soft" color="gray">
+                  閉じる
+                </Button>
+              </AlertDialog.Cancel>
+            </Flex>
+          </AlertDialog.Content>
+        </AlertDialog.Root>
         ))}
       </div>
     </>
