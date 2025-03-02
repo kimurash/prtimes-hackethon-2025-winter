724c1e1ae1c82152fadfb669f0a89d2e9734e0df
shunsei <k.shunsei1227@gmail.com>
2025-03-03 01:42:24 +0900

    Remove unnecessary log

--- frontend/src/pages/dreams/mine/MyDreamCards.tsx
+++ frontend/src/pages/dreams/mine/MyDreamCards.tsx
@@ -75,9 +75,6 @@ const MyDreamCards = ({ myDreams, setMyDreams }: MyDreamCardsProps) => {
                   stroke-width="1.5"
                   stroke="currentColor"
                   className="size-6"
-                  onClick={() => {
-                    console.log("clicked");
-                  }}
                 >
                   <path
                     stroke-linecap="round"
